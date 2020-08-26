0cced98969159510e47307daa0034439ab39307c
sachink4s <67131301+sachink4s@users.noreply.github.com>
2020-08-26 23:13:04 +0530

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -50,10 +50,13 @@ def _check_member(client, message):
       except UserNotParticipant:
         try:
           sent_message = message.reply_text(
-              "{}, you are **not subscribed** to my [channel](https://t.me/{}) yet. Please [join](https://t.me/{}) and **press the button below** to unmute yourself.".format(message.from_user.mention, channel, channel),
+              "Hey [{}](tg://user?id={}),\nYou are **not subscribed** to my [channel](https://t.me/{}) yet. \nPlease [join](https://t.me/{}) and **press the button below** to unmute yourself.".format(message.from_user.mention, channel, channel),
               disable_web_page_preview=True,
               reply_markup=InlineKeyboardMarkup(
-                  [[InlineKeyboardButton("UnMute Me", callback_data="onUnMuteRequest")]]
+                  [
+                   [InlineKeyboardButton(text = 'Join Channel', url="https://t.me/{}")]
+                   [InlineKeyboardButton("UnMute Me", callback_data="onUnMuteRequest")]
+                  ]
               )
           )
           client.restrict_chat_member(chat_id, user_id, ChatPermissions(can_send_messages=False))
