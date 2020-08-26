5ecb928a2bb0a481c2392912831dc4f38b3e101b
sachink4s <67131301+sachink4s@users.noreply.github.com>
2020-08-26 19:58:24 +0530

    Update __init__.py

--- sql_helpers/__init__.py
+++ sql_helpers/__init__.py
@@ -1,7 +1,7 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+from postgresql import create_engine
+from postgresql.ext.declarative import declarative_base
+from postgresql.orm import sessionmaker, scoped_session
 from Config import Config
 
 
