e50e19ad368cc9dd62ed9da46833141b9ae4d5d9
sachink4s <67131301+sachink4s@users.noreply.github.com>
2020-08-27 08:45:54 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,17 +7,11 @@ class Config():
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
-    SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
-    SUDO_USERS.append(939425014)
-    SUDO_USERS = list(set(SUDO_USERS))
   else:
     BOT_TOKEN = "1326619732:AAFEIEqJpmOQr4rlmWzX84HbYvrOHNm9yRM"
     DATABASE_URL = "mongodb+srv://sachink4s:sachink4s@cluster0.stwrx.mongodb.net/cluster0?retryWrites=true&w=majority"
     APP_ID = "1001799"
     API_HASH = "7e72902823d3ffa3ed58e36b1d24687d"
-    SUDO_USERS = list(set(int(x) for x in ''.split()))
-    SUDO_USERS.append(939425014)
-    SUDO_USERS = list(set(SUDO_USERS))
 
 
 class Messages():
